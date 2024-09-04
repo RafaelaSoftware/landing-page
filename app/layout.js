429b046b678b9051671cca3903804f6544b13fe2
Agustin Galvan <agusstingalvan2@gmail.com>
2024-09-04 11:23:28 -0300

    add metatags

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,40 @@
 import ChakraProviderUI from "@/components/providers/ChakraProviderUI";
 
 export const metadata = {
-  title: "LANDING PAGE - RafaelaSoftware",
+  title: process.env.NEXT_PUBLIC_APP_NAME,
   description: "Descripcion de la landing page",
+  authors: [
+    {
+      name: "Rafaela Software S.A.",
+      email: "info@rafaelasoftware.com.ar",
+    }
+  ],
+  keywords: "rafaelasoftware, ERP Calipso Corporate, Sistema MES, Software a medida, Rafaela Software S.A.",
+  image: "/images/logo.svg",
+  url: process.env.NEXT_PUBLIC_APP_URL,
+  type: "website",
+  siteName: "RafaelaSoftware",
+  locale: "es_ES",
+
+
+  // Twitter
+  twitter: {
+    card: "summary_large_image",
+    site: "@rafaelasoftware",
+    images: ["/images/logo.svg"],
+  },
+  robots: "index, follow",
+
+  // Open Graph
+  openGraph: {
+    type: "website",
+    locale: "es_ES",
+    url: process.env.NEXT_PUBLIC_APP_URL,
+    site_name: process.env.NEXT_PUBLIC_APP_NAME,
+    title: process.env.NEXT_PUBLIC_APP_NAME,
+    description: "Descripcion de la landing page",
+    image: "/images/landing-page.jpg",
+  },
 };
 
 export default function RootLayout({ children }) {
